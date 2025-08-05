9c944b6d9f8e50124ef1d5292534b97c5bf8ed3f
AldrichZebua <aldrichderilchristianzebua@gmail.com>
2025-08-05 16:53:24 +0700

    fix layout

--- src/components/layout/BaseLayout.tsx
+++ src/components/layout/BaseLayout.tsx
@@ -1,14 +1,28 @@
-export const BaseLayout = ({ children }: { children: React.ReactNode }) => {
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeController } from "./ThemeController";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+const mainMenu = [
+  { label: "Home", href: "/home" },
+  // { label: "Portfolio", href: "/portfolio" },
+  // { label: "About Me", href: "/aboutme" },
+  { label: "Services", href: "/Service" },
+  { label: "Contact", href: "/contact" },
+];
+
+export const BaseLayout = ({ children }: { children: ReactNode }) => {
+  const pathname = usePathname();
+
   return (
     <div className="min-w-screen-sm max-w-screen-2xl mx-auto">
-      <div className="navbar bg-base-100 shadow-sm">
+      <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle"
-            >
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -20,67 +34,62 @@ export const BaseLayout = ({ children }: { children: React.ReactNode }) => {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h7"
+                  d="M4 6h16M4 12h16M4 18h16"
                 />
               </svg>
-            </div>
+            </label>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <li>
-                <a>Homepage</a>
-              </li>
-              <li>
-                <a>Portfolio</a>
-              </li>
-              <li>
-                <a>About</a>
-              </li>
+              {mainMenu.map((item) => (
+                <li key={item.href}>
+                  <Link
+                    href={item.href}
+                    className={
+                      pathname === item.href ? "font-semibold text-primary" : ""
+                    }
+                  >
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
+
+          <Link href="/" className="flex items-center gap-2 ml-2">
+            <Image
+              src="/asset/my_logo_gold.png"
+              alt="Logo"
+              width={30}
+              height={30}
+            />
+            {/* <span className="text-xl font-bold">Aldrich Zebua</span> */}
+          </Link>
         </div>
-        <div className="navbar-center">
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {mainMenu.map((item) => (
+              <li key={item.href}>
+                <Link
+                  href={item.href}
+                  className={
+                    pathname === item.href ? "font-semibold text-primary" : ""
+                  }
+                >
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
+
         <div className="navbar-end">
-          <button className="btn btn-ghost btn-circle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <button className="btn btn-ghost btn-circle">
-            <div className="indicator">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                />
-              </svg>
-              <span className="badge badge-xs badge-primary indicator-item"></span>
-            </div>
-          </button>
+          <ThemeController />
         </div>
       </div>
+
       <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
         {children}
       </div>
